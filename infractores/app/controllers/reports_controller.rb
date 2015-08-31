b292d27619ab41c22aef8b0c6b730f2dde12a4a2
alanhala <alanhalatian@hotmail.com>
2015-08-31 16:10:42 -0300

    created the reports

---
+++ infractores/app/controllers/reports_controller.rb
@@ -0,0 +1,20 @@
+class ReportsController < ApplicationController
+  before_action :set_report, only: [:show]
+
+  # GET /reports
+  # GET /reports.json
+  def index
+    @reports = Report.all
+  end
+
+  # GET /reports/1
+  # GET /reports/1.json
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_report
+      @report = Report.find(params[:id])
+    end
+end
