6d8f265e20e5cd4cad93c7398c5631e13b4ff385
alanhala <alanhalatian@hotmail.com>
2015-09-01 17:06:46 -0300

    added the has_many relation and the create_or_update_name method

--- infractores/app/models/user.rb
+++ infractores/app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
-  
+  has_many :reports
+
+  def self.create_or_update_name(user)
+    found_user = User.find_or_initialize_by(account_id: user.id)
+    if found_user.new_record?
+      found_user.account_name = user.screen_name
+      found_user.save!
+    elsif found_user.account_name != user.screen_name
+      found_user.update_attribute(:account_name, user.screen_name)
+    end
+    return found_user
+  end
 end
