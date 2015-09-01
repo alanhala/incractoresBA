ad3f85cd6c82174db74a6886da1d7286ea31a073
alanhala <alanhalatian@hotmail.com>
2015-09-01 17:05:58 -0300

    added the tweet_to_invalid_report method and the valid_report scope

--- infractores/app/models/report.rb
+++ infractores/app/models/report.rb
@@ -1,3 +1,14 @@
 class Report < ActiveRecord::Base
-  
+  scope :valid_reports, -> { where(is_valid: true) }
+  belongs_to :user
+
+  def tweet_to_invalid_report(tweet)
+    TWITTER.update("@#{tweet.user.screen_name} Para reportar un infractor,"\
+                   "necesitas una imagen y geolocalizar el tweet. Para mas"\
+                   "informacion: http://placeholder.com", in_reply_to_status: tweet)
+  end
+
+  def valid_report?
+    self.latitude && self.longitude
+  end
 end
