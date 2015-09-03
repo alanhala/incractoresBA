73122b854111010ba62f41fe462a8fb2752bf826
alanhala <alanhalatian@hotmail.com>
2015-09-03 15:46:34 -0300

    send the marker hash to the view to show the map

--- infractores/app/controllers/reports_controller.rb
+++ infractores/app/controllers/reports_controller.rb
@@ -1,20 +1,35 @@
 class ReportsController < ApplicationController
-  before_action :set_report, only: [:show]
+  before_filter :load_report, only: [:show]
+  before_filter :load_images, only: [:show]
 
   # GET /reports
   # GET /reports.json
   def index
     @reports = Report.valid_reports
+    @hash = Gmaps4rails.build_markers(@reports) do |report, marker|
+      marker.lat report.latitude
+      marker.lng report.longitude
+      marker.infowindow report.post
+    end
   end
 
   # GET /reports/1
   # GET /reports/1.json
   def show
+    @hash = Gmaps4rails.build_markers(@report) do |report, marker|
+      marker.lat report.latitude
+      marker.lng report.longitude
+      marker.infowindow report.post
+    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_report
+
+    def load_report
       @report = Report.find(params[:id])
+    end
+
+    def load_images
+      @images = @report.images
     end
 end
