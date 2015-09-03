092f23740ea0d5369ca8e61c588412247be761e5
alanhala <alanhalatian@hotmail.com>
2015-09-03 10:09:20 -0300

    refactored class

--- infractores/app/models/report_builder.rb
+++ infractores/app/models/report_builder.rb
@@ -2,24 +2,39 @@ class ReportBuilder
 
   def self.create_reports(tweets)
     tweets.each do |tweet|
-      params_tweet = self.tweet_params(tweet)
-      user = User.create_or_update_name(tweet.user)
-      params_tweet[:user_id] = user.id
-      report = Report.create(params_tweet)
-      if report.valid_report?
-        report.update_attribute(:is_valid, true)
+      adapted_tweet = TweetAdapter.new(tweet)
+      if adapted_tweet.valid?
+        self.create_valid_report(adapted_tweet)
       else
-        report.tweet_to_invalid_report(tweet)
+        self.create_invalid_report(adapted_tweet)
       end
     end
+  end
+
+  def self.create_valid_report(tweet)
+    user = User.create_or_update_name(tweet.user)
+    report = create_base_report(tweet)
+    report.save!
+    report.attach_images(tweet)
+    report.update_attributes(is_valid: true, user_id: user.id)
+  end
+
+  def self.create_invalid_report(tweet)
+    report = create_base_report(tweet)
+    report.save!
+    #   report.tweet_to_invalid_report(tweet)
+  end
+
+  def self.create_base_report(tweet)
+    Report.new(self.tweet_params(tweet))
   end
 
   def self.tweet_params(tweet)
     params = {}
     params[:tweet_id] = tweet.id
-    params[:latitude] = tweet.geo.coordinates.first
-    params[:longitude] = tweet.geo.coordinates.last
-    params[:post] = tweet.text
+    params[:latitude] = tweet.latitude
+    params[:longitude] = tweet.longitude
+    params[:post] = tweet.post
     return params
   end
 end
