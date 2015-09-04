a265cd0b4cef3cc6b509f8c62acda18ce081308a
alanhala <alanhalatian@hotmail.com>
2015-09-04 15:43:44 -0300

    class refactored. added the possibility to create reports coming from #infractoresba

--- infractores/app/models/report_builder.rb
+++ infractores/app/models/report_builder.rb
@@ -1,40 +1,38 @@
 class ReportBuilder
 
-  def self.create_reports(tweets)
+  def self.create_reports(tweets, source)
     tweets.each do |tweet|
       adapted_tweet = TweetAdapter.new(tweet)
+      report = self.find_or_initialize_base_report(adapted_tweet, source)
+      return if !report.new_record?
       if adapted_tweet.valid?
-        self.create_valid_report(adapted_tweet)
+        self.create_valid_report(adapted_tweet, report)
       else
-        self.create_invalid_report(adapted_tweet)
+        report.save
+        # report.tweet_to_invalid_report(tweet)
       end
     end
   end
 
-  def self.create_valid_report(tweet)
+  def self.create_valid_report(tweet, report)
     user = User.create_or_update_name(tweet.user)
-    report = create_base_report(tweet)
-    report.save!
+    report.save
     report.attach_images(tweet)
     report.update_attributes(is_valid: true, user_id: user.id)
   end
 
-  def self.create_invalid_report(tweet)
-    report = create_base_report(tweet)
-    report.save!
-    #   report.tweet_to_invalid_report(tweet)
+  def self.find_or_initialize_base_report(tweet, source)
+    # Report.new(self.tweet_params(tweet))
+    Report.create_with(self.tweet_params(tweet, source)).find_or_initialize_by(tweet_id: tweet.id)
   end
 
-  def self.create_base_report(tweet)
-    Report.new(self.tweet_params(tweet))
-  end
-
-  def self.tweet_params(tweet)
+  def self.tweet_params(tweet, source)
     params = {}
     params[:tweet_id] = tweet.id
     params[:latitude] = tweet.latitude
     params[:longitude] = tweet.longitude
     params[:post] = tweet.post
+    params[:source] = source
     return params
   end
 end
