954f39fb1da91e1308342c2887b8b84e9652defc
alanhala <alanhalatian@hotmail.com>
2015-09-04 15:49:44 -0300

    fixed spec

--- infractores/spec/models/report_builder_spec.rb
+++ infractores/spec/models/report_builder_spec.rb
@@ -3,14 +3,9 @@ require 'rake'
 
 describe "#create_reports" do
   before do
-    # @client = double("Twitter::REST:Client")
     @user = double("Twitter::User")
     @tweet = double("Twitter::Tweet")
-    # @geo = double("Twitter::Geo::Point")
-    #
-    # allow(@tweet).to receive_messages(user: @user, id: 1, geo: @geo, text: "SARASA")
     allow(@user).to receive_messages(id: 2, screen_name: "example")
-    # allow(@geo).to receive(:coordinates).and_return([2,3])
     allow_any_instance_of(TweetAdapter).to receive_messages(id: 1, latitude: 2,
                                                               longitude: 3,
                                                               post: "SARASA",
@@ -22,7 +17,7 @@ describe "#create_reports" do
 
   it "should create a valid report for the valid tweet" do
     expect(Report.count).to eq(0)
-    ReportBuilder.create_reports([@tweet])
+    ReportBuilder.create_reports([@tweet], "mention")
     expect(Report.count).to eq(1)
     report = Report.first
     expect(report.latitude).to eq(2)
@@ -32,7 +27,9 @@ describe "#create_reports" do
   end
 
   it "should create two valid reports for the same user" do
-    ReportBuilder.create_reports([@tweet, @tweet])
+    ReportBuilder.create_reports([@tweet], "mention")
+    allow_any_instance_of(TweetAdapter).to receive(:id).and_return(2)
+    ReportBuilder.create_reports([@tweet], "mention")
     expect(Report.valid_reports.count).to eq(2)
     expect(User.count).to eq(1)
     expect(User.last.reports.count).to eq(2)
@@ -40,7 +37,7 @@ describe "#create_reports" do
 
   it "should not create an user if the tweet is not valid but creates an invalid report" do
     allow_any_instance_of(TweetAdapter).to receive(:valid?).and_return(false)
-    ReportBuilder.create_reports([@tweet])
+    ReportBuilder.create_reports([@tweet], "mention")
     expect(Report.valid_reports.count).to eq(0)
     expect(Report.count).to eq(1)
     expect(User.count).to eq(0)
