b90fcdd4c233202dea93b950ddb53e0ff9cbd226
alanhala <alanhalatian@hotmail.com>
2015-09-04 15:47:10 -0300

    refactored create_or_update_name method

--- infractores/app/models/user.rb
+++ infractores/app/models/user.rb
@@ -2,13 +2,10 @@ class User < ActiveRecord::Base
   has_many :reports
 
   def self.create_or_update_name(user)
-    found_user = User.find_or_initialize_by(account_id: user.id)
-    if found_user.new_record?
-      found_user.account_name = user.screen_name
-      found_user.save!
-    elsif found_user.account_name != user.screen_name
+    found_user = User.create_with(account_name: user.screen_name).find_or_create_by(account_id: user.id)
+    if found_user.account_name != user.screen_name
       found_user.update_attribute(:account_name, user.screen_name)
     end
-    return found_user
+    found_user
   end
 end
