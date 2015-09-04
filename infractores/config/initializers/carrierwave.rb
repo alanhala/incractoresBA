0b409024b0c89417bd5eacec0248ff3ba1992598
alanhala <alanhalatian@hotmail.com>
2015-09-04 15:49:09 -0300

    carrierwave don’t use S3 when env is test

--- infractores/config/initializers/carrierwave.rb
+++ infractores/config/initializers/carrierwave.rb
@@ -1,10 +1,17 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],
-    :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],
-    :region                 => "us-east-1"
-  }
-  config.cache_dir = "#{Rails.root}/tmp/uploads"
-  config.fog_directory  = "infracciones-ba-#{Rails.env}"
+  if Rails.env.test? || Rails.env.cucumber?
+    config.storage = :file
+    config.enable_processing = false
+    config.root = "#{Rails.root}/tmp"
+  else
+    config.storage = :fog
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],
+      :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],
+      :region                 => "us-east-1"
+    }
+    config.cache_dir = "#{Rails.root}/tmp/uploads"
+    config.fog_directory  = "infracciones-ba-#{Rails.env}"
+  end
 end
